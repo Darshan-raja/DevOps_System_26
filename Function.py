4a1ddd5dda3e56c4578f43619817e9dc5de74f75
darshan <darshanraja576@gmail.com>
2025-07-28 00:05:38 +0530

    e-commerce

--- Function.py
+++ Function.py
@@ -9,10 +9,28 @@
 # result = get_sum(456 ,56464)
 # print("The sum is", result)
 
-def get_sum(number1, number2): #declaration of the function
-     sum = number1 + number2 #definition of the function
-     return sum;
+# def get_sum(number1, number2): #declaration of the function
+#      sum = number1 + number2 #definition of the function
+#      return sum;
 
-#calling the function
-get = get_sum(5665, 646546)
-print("the sum is", get)
+# #calling the function
+# get = get_sum(5665, 646546)
+# print("the sum is", get)
+#print("custom sum:", get_sum(654,546))
+
+#ef get_sum(*Number):
+# def get_sum(**kwargs): #kwargs,*Number is a keyword argument
+#      return sum(kwargs.values())
+#           # Calling the function with multiple arguments
+# print("Sum from keyword args:", get_sum(a=5, b=5, c=20))
+
+#E-Commerce example using Functions
+def E_commerce(cart_item, tax):
+     total_item = sum(cart_item)
+     tax_price = total_item * tax
+     total_amount = total_item +tax_price
+     return total_amount
+
+cart_items = [20, 30, 40,]
+print("Total amount after tax:", E_commerce(cart_items, 0.05))
+
