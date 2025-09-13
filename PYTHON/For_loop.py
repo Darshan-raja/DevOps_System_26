e1210bfe493628c374d88961d2298cd2538817fe
darshan <darshanraja576@gmail.com>
2025-09-13 23:45:45 +0530

    dsa Practice

--- PYTHON/For_loop.py
+++ PYTHON/For_loop.py
@@ -13,7 +13,8 @@
 #     print(i)
 # i will take value from 0 to 4
 #  is is a variable
-# "range"(5\) is built-in function will generate n numbers
+# "range"(5\) is built-in function will generate n numbers it expect integer value
+# range(start, stop, step)
 # for{vaiable name} in {iterable} in {collection}:
 # {collection} is a list, tuple, set, dictionary, string
 # {iterable} is a sequence of elements that can be iterated over
@@ -23,16 +24,23 @@
 
 # print(value.count(4))
 
-# for d in range(10, 5, -10):
-#     # range(start, stop, step)
-#     # start = 10: The first number in the sequence.
-#     # stop = 5: The sequence stops before this number.
-#     # step = -10: The sequence counts down by 10.
-#     # The loop will only run once for the value 10.
+# for d in range(10, 5, 10):
+# range(start, stop, step)
+# start = 10: The first number in the sequence.
+# stop = 5: The sequence stops before this number.
+# step = 10 increment by 10 each time.
+# The loop will only run once for the value 10.
 #     print(d)
+# for er in range(11, 11, -2):
+#     # 0 start value
+#     # 10 stop value
+#     # 2 step value increment by 2
+#     print(er)
 
 
-for i in range(1, 5):  # Start a for loop that iterates through numbers from 1 to 4 (inclusive)
-    for k in range(1, 10):  # Iterate through numbers from 1 to 9 (inclusive)
-        print(k, end='')
-    print()  
+cars = ["TATA", "ford", "BMW", "AUdi"]  # list of cars
+for car in range(len(cars)):
+    cars.append("Rolls Royece")
+    cars.insert(3, "Lamborghini")
+    print(cars)
+    
